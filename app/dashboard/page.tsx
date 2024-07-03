307d712c1b81528cb8f572ae9aafc3523e131d59
efoneli <abbaefoneli@gmail.com>
2024-07-03 21:46:47 +0100

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+        <Header />
+
+        <h1>body here</h1>
+    </div>
+  )
+}
